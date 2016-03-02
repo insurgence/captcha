bc2fc25a0f0505381bfe6dc3c75b7872a74a8f87
vadim <insurgence@mail.ru>
2016-03-02 22:42:48 +0400

    Get exif information from image

--- captcha/Program.cs
+++ captcha/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using ImageMagick;
 
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace captcha
 {
@@ -12,13 +12,29 @@ namespace captcha
     {
         static void Main(string[] args)
         {
-            MagickImageInfo info = new MagickImageInfo(@"C:\ph.png");
+            MagickImageInfo info = new MagickImageInfo(@"C:\DSC_6919v16.jpg");
             Console.WriteLine(info.Format);
             Console.WriteLine(info.Width);
             Console.WriteLine(info.Height);
             Console.WriteLine(info.ColorSpace);
             Console.WriteLine(info.Density.X);
             Console.WriteLine(info.Density.Y);
+
+            Console.WriteLine("Read Exif Data: ");
+            using (MagickImage image = new MagickImage(@"C:\DSC_6919v16.jpg"))
+            {
+                ExifProfile profile = image.GetExifProfile();
+
+                if (profile == null)
+                    Console.WriteLine("Image does not contain exif information");
+                else
+                {
+                    foreach(ExifValue value in profile.Values)
+                    {
+                        Console.WriteLine("{0}({1}) : {2}", value.Tag, value.DataType, value.ToString());
+                    }
+                }
+            }
             Console.ReadLine();
         }
     }
